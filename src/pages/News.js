76e08a06f07c40a19ff7d332deb261f9449571b2
daeun <dems9811@goldenplanet.co.kr>
2024-08-13 23:38:44 +0900

    Pagination Updated

--- src/pages/News.js
+++ src/pages/News.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import ReactPaginate from 'react-paginate';
 import axios from 'axios';
 // import { format, subDays } from 'date-fns'; // 날짜 처리를 위해 date-fns 사용
 import { useNavigate, useLocation } from 'react-router-dom'; // 페이지네이션을 위한 react-router-dom 사용
@@ -14,14 +15,19 @@ const NewsPage = () => {
   const initialPage = parseInt(queryParams.get('page')) || 1;
 
   const [articles, setArticles] = useState([]); // 뉴스 기사 목록을 저장할 상태 변수
+  const [pageCount, setPageCount] = useState(0);
   const [page, setPage] = useState(initialPage); // 현재 페이지 번호를 저장할 상태 변수
   // const [date, setDate] = useState(format(subDays(new Date(), 1), 'yyyy-MM-dd')); // 어제 날짜 설정
 
   const searchQuery = useSelector((state) => state.search.searchQuery) || 'Apple'; // Redux에서 검색어 가져오기, searchQuery가 변경될 때만 API를 호출하기
   const searchDate = useSelector((state) => state.search.searchDate);
+
   const pageSize = 10; // 한 페이지에 표시할 기사 수
+  const maxPage = 10;
   const apiKey = process.env.REACT_APP_API_KEY; // 환경 변수에서 API 키 가져오기
 
+
+  //[질문] 이런경우 전역상태에서 API 호출 기능을 관리하는지?!
   useEffect(() => {
     const fetchNews = async (query, date, page) => {
       try {
@@ -36,6 +42,8 @@ const NewsPage = () => {
           }
         });
         setArticles(response.data.articles);
+        setPageCount(Math.ceil(response.data.totalResults / pageSize));
+
       } catch (error) {
         console.error('Error fetching news:', error);
         if (error.response) {
@@ -47,9 +55,24 @@ const NewsPage = () => {
     fetchNews(searchQuery, searchDate, page); // 페이지, 날짜 또는 검색어가 변경될 때마다 API 호출(ferchNews 함수 호출)
   }, [searchQuery, searchDate, page, apiKey]); // searchQuery 상태도 디펜던시로 추가
 
-  const handlePageChange = (newPage) => {
-    setPage(newPage);
-    navigate(`?page=${newPage}`); // URL에 페이지 번호를 반영
+  const handlePageClick = (data) => {
+    const selectedPage = data.selected + 1;
+    setPage(selectedPage);
+    navigate(`?page=${selectedPage}`);
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  };
+
+
+  const handleFirstPage = () => {
+    setPage(1);
+    navigate('?page=1');
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  };
+
+  const handleLastPage = () => {
+    setPage(pageCount);
+    navigate(`?page=${pageCount}`);
+    window.scrollTo({ top: 0, behavior: 'smooth' });
   };
 
   return (
@@ -69,36 +92,34 @@ const NewsPage = () => {
             </div>
           ))}
         </div>
-        <div className="pagination"> {/* 페이지네이션 */}
-          {/* {pageCount > 0 && (
-          <Pagination
-            pageCount={Math.max(1, pageCount - 1)}
-            onPageChange={handlePageChange}
-            currentPage={currentPage}
-          />
-        )} */}
+        <div className="pagination-wrapper">
           <button 
+            onClick={handleFirstPage} 
+            disabled={page === 1} 
             className="pagination-button"
-            onClick={() => handlePageChange(page - 1)} 
-            disabled={page === 1}
           >
-            Previous
+            First
           </button>
-          {[...Array(5)].map((_, index) => (
-            <button 
-              key={index} 
-              className={page === index + 1 ? 'active' : ''}
-              onClick={() => handlePageChange(index + 1)}
-            >
-              {index + 1}
-            </button>
-          ))}
+          <ReactPaginate
+            previousLabel={'<'}
+            nextLabel={'>'}
+            breakLabel={null}
+            pageCount={Math.min(pageCount, maxPage)}
+            marginPagesDisplayed={0}
+            pageRangeDisplayed={5}
+            onPageChange={handlePageClick}
+            containerClassName={'pagination'}
+            pageClassName={'pagination-item'}
+            pageLinkClassName={'pagination-link'} 
+            activeClassName={'active'}
+            forcePage={page - 1}
+          />
           <button 
+            onClick={handleLastPage} 
+            disabled={page === pageCount} 
             className="pagination-button"
-            onClick={() => handlePageChange(page + 1)} 
-            disabled={page === 5}
           >
-            Next
+            Last
           </button>
         </div>
       </main>
